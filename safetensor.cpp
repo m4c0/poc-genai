51cb3d3c75a509d4ac3b96500e9dff7e4e669bc6
Eduardo Costa <m4c0@users.noreply.github.com>
2024-12-14 20:22:49 +0000

    reading header from a safetensor

---
+++ safetensor.cpp
@@ -0,0 +1,25 @@
+#pragma leco tool
+
+import jojo;
+import jute;
+import print;
+import traits;
+
+using namespace traits::ints;
+
+int main(int argc, char ** argv) try {
+  if (argc != 2) die("missing filename");
+
+  auto model_raw = jojo::read(jute::view::unsafe(argv[1]));
+  jute::view model { model_raw.begin(), model_raw.size() };
+
+  auto hdr_size = *reinterpret_cast<const uint64_t *>(model.begin());
+  auto [sz, hdr, cnt] = model.subview(4, hdr_size);
+
+  if (hdr_size != hdr.size())
+    die("invalid safetensor - expecting header with size ", hdr_size, ", got ", hdr.size());
+  
+  putln(hdr);
+} catch (...) {
+  return 1;
+}
