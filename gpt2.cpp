441e092747d04081cb672e6aa0d305c4b2f0985e
Eduardo Costa <m4c0@users.noreply.github.com>
2024-12-17 13:21:09 +0000

    a float32 view

--- gpt2.cpp
+++ gpt2.cpp
@@ -17,6 +17,14 @@ template<unsigned W, unsigned H>
 struct f32a {
   hai::array<float> data { W * H };
 };
+template<unsigned W, unsigned H>
+struct f32v {
+  const float * data {};
+
+  f32v() = default;
+  explicit f32v(const float * d) : data { d } {}
+  f32v(f32a<W, H> & a) : data { a.data.begin() } {}
+};
 
 static constexpr const auto n_ctx = 1024;
 static constexpr const auto n_embed = 768;
@@ -49,7 +57,8 @@ static void debug(const float *xx, int r, int c) {
 static void debug(const f32a & x, int r, int c) { debug(x.begin(), r, c); }
 #endif
 
-static const float * extract(jute::view key) {
+template<unsigned W, unsigned H>
+static f32v<W, H> extract(jute::view key) {
   auto & root = *g_config;
   auto & v = j::cast<jn::dict>(root[key]);
   auto dtype = j::cast<jn::string>(v["dtype"]).str();
@@ -67,15 +76,16 @@ static const float * extract(jute::view key) {
   if (end < start || end - start > g_cnt.size()) die("invalid offsets ", start, "~", end);
 
   // unsigned len = end - start;
-  return reinterpret_cast<const float *>(g_cnt.begin() + start);
+  return f32v<W, H>{ reinterpret_cast<const float *>(g_cnt.begin() + start) };
 }
+template<unsigned W, unsigned H>
 static auto extract(int layer, const char * a, const char * b) {
   char buf[1024] {};
   snprintf(buf, sizeof(buf), "h.%d.%s.%s", layer, a, b);
-  return extract(jute::view::unsafe(buf));
+  return extract<W, H>(jute::view::unsafe(buf));
 }
 
-static auto layer_norm(f32a<n_ctx, n_embed> & x, unsigned tks, const float * weight, const float * bias) {
+static auto layer_norm(f32a<n_ctx, n_embed> & x, unsigned tks, f32v<n_embed, 1> weight, f32v<n_embed, 1> bias) {
   f32a<n_ctx, n_embed> res {};
 
   for (auto i = 0; i < tks; i++) {
@@ -95,35 +105,36 @@ static auto layer_norm(f32a<n_ctx, n_embed> & x, unsigned tks, const float * wei
     variance /= n_embed;
     variance += n_eps;
 
+    // g * x + b
     auto res_ptr = &res.data[i * n_embed];
     for (auto j = 0; j < n_embed; j++) {
-      res_ptr[j] = weight[j] * (x_ptr[j] - mean) / dotz::sqrt(variance) + bias[j];
+      res_ptr[j] = weight.data[j] * (x_ptr[j] - mean) / dotz::sqrt(variance) + bias.data[j];
     }
   }
 
   return res;
 }
 static auto layer_norm(f32a<n_ctx, n_embed> & x, unsigned tks, int layer, const char * ln) {
-  auto bias = extract(layer, ln, "bias");
-  auto weight = extract(layer, ln, "weight");
+  auto bias = extract<n_embed, 1>(layer, ln, "bias");
+  auto weight = extract<n_embed, 1>(layer, ln, "weight");
   return layer_norm(x, tks, weight, bias);
 }
 
-// x @ w + b;
+// x @ w + b -- cij = aik * bkj
 template<unsigned I, unsigned J, unsigned K>
-static auto linear(f32a<I, K> & x, int layer, const char * mats, const float * init) {
-  auto w = extract(layer, mats, "weight");
-  auto b = extract(layer, mats, "bias");
+static auto linear(f32a<I, K> & x, int layer, const char * mats, f32v<I, J> init) {
+  auto w = extract<K, J>(layer, mats, "weight");
+  auto b = extract<K, 1>(layer, mats, "bias");
 
   f32a<I, J> res {};
   auto ptr = res.data.begin();
   for (auto i = 0; i < I; i++) {
     auto x_ptr = &x.data[i * K];
     for (auto j = 0; j < J; j++, ptr++) {
-      *ptr = init ? init[i * J + j] : 0;
-      *ptr += b[j];
+      *ptr = init.data ? init.data[i * J + j] : 0;
+      *ptr += b.data[j];
       for (auto k = 0; k < K; k++) {
-        *ptr += x_ptr[k] * w[k * J + j];
+        *ptr += x_ptr[k] * w.data[k * J + j];
       }
     }
   }
@@ -132,7 +143,7 @@ static auto linear(f32a<I, K> & x, int layer, const char * mats, const float * i
 
 static auto mha(f32a<n_ctx, n_embed> & x, unsigned tks, int layer) {
   auto xn = layer_norm(x, tks, layer, "ln_1");
-  auto qkv = linear<n_ctx, n_embed * 3, n_embed>(xn, layer, "attn.c_attn", nullptr);
+  auto qkv = linear<n_ctx, n_embed * 3, n_embed>(xn, layer, "attn.c_attn", {});
 
   f32a<n_ctx, n_embed> hstack {};
   f32a<n_ctx, n_ctx> smax {};
@@ -186,19 +197,19 @@ static auto mha(f32a<n_ctx, n_embed> & x, unsigned tks, int layer) {
     }
   }
 
-  return linear<n_ctx, n_embed, n_embed>(hstack, layer, "attn.c_proj", x.data.begin());
+  return linear<n_ctx, n_embed, n_embed>(hstack, layer, "attn.c_proj", x);
 }
 
 static constexpr const auto pi = 3.14159265358979323;
 static auto ffn(f32a<n_ctx, n_embed> & x, int tks, int layer) {
   auto xn = layer_norm(x, tks, layer, "ln_2");
-  auto a = linear<n_ctx, n_embed * 4, n_embed>(xn, layer, "mlp.c_fc", nullptr);
+  auto a = linear<n_ctx, n_embed * 4, n_embed>(xn, layer, "mlp.c_fc", {});
   for (auto & f : a.data) {
     using namespace dotz;
     f = 0.5 * f * (1 + tanh(sqrt(2.0 / pi) * (f + 0.044715 * f * f * f)));
   }
 
-  return linear<n_ctx, n_embed, n_embed * 4>(a, layer, "mlp.c_proj", x.data.begin());
+  return linear<n_ctx, n_embed, n_embed * 4>(a, layer, "mlp.c_proj", x);
 }
 
 static void print_token(const auto & vocab, int tk) {
@@ -249,18 +260,18 @@ int main(int argc, char ** argv) try {
   for (auto i = 0; i < tks; i++) print_token(vocab, in_tks[i]);
 
   // TODO: assert wpe/wte sizes
-  auto wte = extract("wte.weight");
-  auto wpe = extract("wpe.weight");
-  auto lnf_b = extract("ln_f.bias");
-  auto lnf_w = extract("ln_f.weight");
+  auto wte = extract<n_vocab, n_embed>("wte.weight");
+  auto wpe = extract<n_ctx, n_embed>("wpe.weight");
+  auto lnf_b = extract<n_embed, 1>("ln_f.bias");
+  auto lnf_w = extract<n_embed, 1>("ln_f.weight");
 
   for (; tks < in_tks.size(); tks++) {
     f32a<n_ctx, n_embed> x {};
 
     // x = wte[token_ids] + wpe[[0, 1, 2...]]
     for (auto i = 0; i < tks; i++) {
-      auto wte_ptr = &wte[in_tks[i] * n_embed];
-      auto wpe_ptr = &wpe[i * n_embed];
+      auto wte_ptr = &wte.data[in_tks[i] * n_embed];
+      auto wpe_ptr = &wpe.data[i * n_embed];
       auto x_ptr   = &x.data[i * n_embed];
       for (auto j = 0; j < n_embed; j++) {
         x_ptr[j] = wte_ptr[j] + wpe_ptr[j];
@@ -282,7 +293,7 @@ int main(int argc, char ** argv) try {
       for (auto j = 0; j < n_vocab; j++) {
         float n = 0;
         for (auto k = 0; k < n_embed; k++) {
-          n += x_ptr[k] * wte[j * n_embed + k];
+          n += x_ptr[k] * wte.data[j * n_embed + k];
         }
         if (n > max) {
           max = n;
