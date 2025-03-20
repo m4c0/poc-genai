2d2ba17db6203a3dfed43fb38ce10269a6d89249
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-20 23:00:46 +0000

    openai: tool for listing files

--- openai.py
+++ openai.py
@@ -15,56 +15,67 @@ headers = {
   'Content-Type': 'application/json'
 }
 
-data = json.dumps({
-  "model": "gpt-4o-mini",
-  "messages": [{
-    "role": "developer",
-    "content": "You are an automated assistant robot without personality"
-  }, {
-    "role": "user",
-    "content": "List files in this repo"
-  }],
-  "tools": [{
-    "type": "function",
-    "function": {
-      "name": "list_files",
-      "description": "list files in current directory",
-      "strict": True,
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "path": {
-            "type": "string",
-            "description": "path to search"
-          }
-        },
-        "required": ["path"],
-        "additionalProperties": False
-      }
-    },
-  }]
-})
+messages = [{
+  "role": "developer",
+  "content": "You are an automated assistant robot without personality"
+}, {
+  "role": "user",
+  "content": "List shader files"
+}]
 
-conn.request("POST", "/v1/chat/completions", data, headers)
-resp = conn.getresponse()
-resp_data = resp.read()
-conn.close()
+while True:
+  data = json.dumps({
+    "model": "gpt-4o-mini",
+    "messages": messages,
+    "tools": [{
+      "type": "function",
+      "function": {
+        "name": "list_files",
+        "description": "list files in current directory",
+        "strict": True,
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "path": {
+              "type": "string",
+              "description": "path to search"
+            }
+          },
+          "required": ["path"],
+          "additionalProperties": False
+        }
+      },
+    }]
+  })
+  
+  conn.request("POST", "/v1/chat/completions", data, headers)
+  resp = conn.getresponse()
+  resp_data = resp.read()
+  conn.close()
+  
+  if resp.status != 200:
+    raise Exc(f"Error: {resp.status}, {resp_data.decode()}")
+  
+  cho = json.loads(resp_data)['choices'][0]
+  msg = cho['message']
+  
+  class Funcs:
+    def list_files(path):
+      print(f"listing {path}")
+      return os.listdir(".")
+  
+  if msg['content']:
+    print(msg['content'])
 
-if resp.status != 200:
-  raise Exc(f"Error: {resp.status}, {resp_data.decode()}")
-
-msg = json.loads(resp_data)['choices'][0]['message']
-
-class Funcs:
-  def list_files(path):
-    print(f"listing {path}")
-    return os.listdir(".")
-
-if msg['content']:
-  print(msg['content'])
-
-for tc in msg['tool_calls']:
-  fn = tc['function']
-  val = getattr(Funcs, fn['name'])(*json.loads(fn['arguments']))
-  print(val)
+  if (cho['finish_reason'] == 'stop'):
+    break
+  
+  messages.append(msg)
+  for tc in msg['tool_calls']:
+    fn = tc['function']
+    messages.append({
+      "role": "tool",
+      "tool_call_id": tc["id"],
+      "content": str(getattr(Funcs, fn['name'])(*json.loads(fn['arguments'])))
+    })
 
