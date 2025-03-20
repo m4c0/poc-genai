4138a26b5256093dbf76ca4a481e70faf89e9af4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-20 22:05:12 +0000

    openai example in python

---
+++ openai.py
@@ -0,0 +1,43 @@
+import http.client
+import json
+import os
+
+class Exc(Exception):
+  pass
+
+with open(os.path.expanduser("~/.openai"), 'r') as file:
+  api_key = file.read().strip()
+
+conn = http.client.HTTPSConnection("api.openai.com")
+
+headers = {
+  'Authorization': f'Bearer {api_key}',
+  'Content-Type': 'application/json'
+}
+
+data = json.dumps({
+  "model": "gpt-4o-mini",
+  "messages": [{
+    "role": "developer",
+    "content": "You are an automated assistant robot without personality"
+  }, {
+    "role": "user",
+    "content": "List files in this repo"
+  }]
+})
+
+conn.request("POST", "/v1/chat/completions", data, headers)
+resp = conn.getresponse()
+resp_data = resp.read()
+conn.close()
+
+if resp.status != 200:
+  raise Exc(f"Error: {resp.status}, {resp_data.decode()}")
+
+resp_json = json.loads(resp_data)['choices'][0]
+print(resp_json)
+
+cont = resp_json['message']['content']
+print(cont)
+
+
