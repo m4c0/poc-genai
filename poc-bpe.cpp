c855eb89c3269da402d05b6ab2f2ece3bdc63b1b
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-16 15:20:21 +0100

    bpe: optimising the compression a bit

--- poc-bpe.cpp
+++ poc-bpe.cpp
@@ -1,4 +1,5 @@
 #pragma leco tool
+#define DUMP_TEST 0
 import hai;
 import hashley;
 import jojo;
@@ -12,7 +13,22 @@ struct pair {
 struct max_compression_reached {};
 
 using dict = hai::chain<pair>;
-using tk_str = hai::chain<unsigned>;
+using tk_str = hai::varray<unsigned>;
+
+#if DUMP_TEST
+static void dump_token(const dict & d, unsigned c) {
+  if (c < 256) put((char) c);
+  else {
+    auto [a, b] = d.seek(c);
+    dump_token(d, a);
+    dump_token(d, b);
+  }
+}
+static void dump(const tk_str & str, const dict & d) {
+  for (auto c : str) dump_token(d, c);
+  putln();
+}
+#endif
 
 static auto create_initial_tokens() {
   dict tokens { 102400 };
@@ -21,7 +37,7 @@ static auto create_initial_tokens() {
 }
 
 static auto convert_to_pair_indices(jute::view str) {
-  tk_str pairs { 102400 };
+  tk_str pairs { static_cast<unsigned>(str.size()) };
   for (auto c : str) pairs.push_back(c);
   return pairs;
 }
@@ -39,7 +55,7 @@ static auto find_next_pair(const tk_str & str) {
   unsigned max_id {};
   unsigned max_count {};
   for (auto i = 0; i < str.size() - 1; i++) {
-    pair key { str.seek(i), str.seek(i + 1) };
+    pair key { str[i], str[i + 1] };
     auto & idx = idxs[(key.a << 16) | key.b];
     if (idx == 0) {
       items.push_back({ key });
@@ -55,53 +71,38 @@ static auto find_next_pair(const tk_str & str) {
   return items.seek(max_id - 1).key;
 }
 
-static auto compress(const tk_str & old, pair p, unsigned idx) {
-  tk_str res { old.size() };
-  for (auto i = 0; i < old.size(); i++) {
-    auto a = old.seek(i);
-    if (a == p.a && i < old.size() - 1 && old.seek(i + 1) == p.b) {
-      res.push_back(idx);
+static void compress(tk_str & old, pair p, unsigned idx) {
+  auto o = 0;
+  for (auto i = 0; i < old.size(); i++, o++) {
+    auto a = old[i];
+    if (i < old.size() - 1 && a == p.a && old[i + 1] == p.b) {
+      old[o] = idx;
       i++;
     } else {
-      res.push_back(a);
+      old[o] = old[i];
     }
   }
-  return res;
+  old.truncate(o);
 }
 
-static auto run_one_compress(const tk_str & str, dict & d) {
+static void run_one_compress(tk_str & str, dict & d) {
   auto pair = find_next_pair(str);
   d.push_back(pair);
-  return compress(str, pair, d.size() - 1);
+  compress(str, pair, d.size() - 1);
 }
 
 static auto run_compression(jute::view in, dict & d) {
   auto str = convert_to_pair_indices(in);
   while (true) try {
-    str = run_one_compress(str, d);
+    run_one_compress(str, d);
     putln(str.size(), "\t\t", d.size());
   } catch (max_compression_reached) {
     return str;
   }
 }
 
-#if 0
-static void dump_token(const dict & d, unsigned c) {
-  if (c < 256) put((char) c);
-  else {
-    auto [a, b] = d.seek(c);
-    dump_token(d, a);
-    dump_token(d, b);
-  }
-}
-static void dump(const tk_str & str, const dict & d) {
-  for (auto c : str) dump_token(d, c);
-  putln();
-}
-#endif
-
 int main() {
-#if 0
+#if 1
   auto cstr = jojo::read_cstr("dom-casmurro.txt");
   jute::view all { cstr };
 #else
@@ -110,5 +111,7 @@ int main() {
 
   auto tokens = create_initial_tokens();
   auto str = run_compression(all, tokens);
-  //dump(str, tokens);
+#if DUMP_TEST
+  dump(str, tokens);
+#endif
 }
