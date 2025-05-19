238cedc197bd589479a505537077affd439acdbb
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-19 13:48:06 +0100

    bpe: recursive print

--- poc-bpe2.cpp
+++ poc-bpe2.cpp
@@ -19,21 +19,29 @@ class pairs {
   hai::chain<pair> m_data { 102400 };
 
 public:
-  void ping(unsigned a, unsigned b) {
+  unsigned ping(unsigned a, unsigned b) {
     uint64_t key = (static_cast<uint64_t>(a) << 32) | b;
     auto & idx = m_idx[key];
     if (idx) {
       m_data.seek(idx - 1).count++;
-      return;
+    } else {
+      m_data.push_back({ a, b, 1 });
+      idx = m_data.size();
     }
-
-    m_data.push_back({ a, b, 1 });
-    idx = m_data.size();
+    return idx - 1;
   }
 
   auto begin() const { return m_data.begin(); }
   auto end() const { return m_data.end(); }
   auto size() const { return m_data.size(); }
+
+  auto count_of(unsigned idx) const { return m_data.seek(idx).count; }
+  void put(unsigned idx) const {
+    auto [a, b, c] = m_data.seek(idx);
+    if (b == 0 && 32 <= a && a <= 127) putf("%c", a);
+    else if (b == 0) putf("\\x%02X", a);
+    else { put(a); put(b); }
+  }
 };
 
 int main() {
@@ -46,10 +54,9 @@ int main() {
     ps.ping(c & 0xFF, 0);
   }
 
-  for (auto [a, b, c]: ps) {
-    if (b != 0) putln(a, " ", b, " ", c);
-    else if (32 <= a && a <= 127) putfn("%c %d %d", a, b, c);
-    else putfn("x%02X %d %d", a, b, c);
+  for (auto i = 0; i < ps.size(); i++) {
+    ps.put(i);
+    putln(" ", ps.count_of(i));
   }
   putfn(">>>>>>>>>>>>>> %d pairs", ps.size());
 }
