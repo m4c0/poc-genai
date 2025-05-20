d881acffa1678dfaeb29fe995a7093f397e9732e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-20 12:38:26 +0100

    bpe: fooling around the idea of generating text

--- poc-bpe2.cpp
+++ poc-bpe2.cpp
@@ -19,12 +19,6 @@ class pairs {
   hai::chain<pair> m_data { 102400 };
 
   auto count_of(unsigned idx) const { return m_data.seek(idx).count; }
-  void print(unsigned idx) const {
-    auto [a, b, c] = m_data.seek(idx);
-    if (b == 0 && 32 <= a && a <= 127) putf("%c", a);
-    else if (b == 0) putf("\\x%02X", a);
-    else { print(a); print(b); }
-  }
 
 public:
   unsigned ping(unsigned a, unsigned b) {
@@ -44,6 +38,18 @@ public:
   auto end() const { return m_data.end(); }
   auto size() const { return m_data.size(); }
 
+  int index_of(unsigned a, unsigned b) const {
+    uint64_t key = (static_cast<uint64_t>(a) << 32) | b;
+    return m_idx[key] - 1;
+  }
+  int index_of(char c) const { return index_of(static_cast<unsigned>(c) & 0xFF, 0); }
+
+  void print(unsigned idx) const {
+    auto [a, b, c] = m_data.seek(idx);
+    if (b == 0 && 32 <= a && a <= 127) putf("%c", a);
+    else if (b == 0) putf("\\x%02X", a);
+    else { print(a); print(b); }
+  }
   void dump(int min_count) const {
     for (auto i = 0; i < size(); i++) {
       if (count_of(i) < min_count) continue;
@@ -61,7 +67,7 @@ int main() {
 
   pairs ps {};
 
-#if 1
+#if 0
   auto a = ps.ping(all[0]);
   for (auto i = 1; i < all.size(); i++) {
     auto b = ps.ping(all[i]);
@@ -69,6 +75,7 @@ int main() {
     a = b;
   }
 #else
+  // TODO: refactor into a Markov model
   auto a = ps.ping(all[0]);
   auto b = ps.ping(all[1]);
   auto ab = ps.ping(a, b);
@@ -83,5 +90,20 @@ int main() {
   }
 #endif
 
-  ps.dump(100);
+  putfn("%d pairs in total", ps.size());
+  //ps.dump(100);
+
+  auto i = ps.index_of(ps.index_of('P'), ps.index_of('a'));
+  ps.print(i);
+  for (auto n = 0; n < 5; n++) {
+    for (auto [a, b, c]: ps) {
+      if (a != i) continue;
+      // TODO: use histogram
+      ps.print(b);
+      i = b;
+      break;
+    }
+    if (i == 0) break;
+  }
+  putln();
 }
